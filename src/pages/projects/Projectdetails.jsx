d64517a4f80cabfb4e3eb8ae7701e88db0e8d504
Your Name <your-email@example.com>
2025-12-24 09:35:32 +0530

    few chnages pedning

--- src/pages/projects/Projectdetails.jsx
+++ src/pages/projects/Projectdetails.jsx
@@ -10,12 +10,9 @@ const Projectdetails = ({ name, description, link, image, CloseModel }) => {
             <motion.div className='relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-t from-midnight to-navy border-white/10 p-4'
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }} >
-                {/* <div className='flex items-center justify-between'>
-                    <p className='subtext'> Project Details  </p> */}
                 <button className='absolute p-1 rounded-sm top-3 right-3 bg-midnight hover:bg-gray-500 cursor-pointer' onClick={CloseModel}>
                     <img src={closeicon} className='w-5 h-5' alt={name} />
                 </button>
-                {/* </div> */}
 
                 <div>
                     <img src={image} className='w-full h-60 object-contain' />
